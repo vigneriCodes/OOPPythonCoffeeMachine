9057f3bc8e8f7210c9e19c10ea499a7c77632e35
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-09 18:36:00 -0800

    change var names/remove imported MenuItem

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
@@ -10,13 +10,13 @@ power_state = True
 
 while power_state:
     options = menu.get_items()
-    order = input(f"What would you like? ({options}): ")
-    if order == "off":
+    prompt_answer = input(f"What would you like? ({options}): ")
+    if prompt_answer == "off":
         power_state = False
-    elif order == "report":
+    elif prompt_answer == "report":
         coffee_maker.report()
         money_machine.report()
     else:
-        drink = menu.find_drink(order)
-        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            coffee_maker.make_coffee(drink)
+        drink_ordered = menu.find_drink(prompt_answer)
+        if coffee_maker.is_resource_sufficient(drink_ordered) and money_machine.make_payment(drink_ordered.cost):
+            coffee_maker.make_coffee(drink_ordered)
