66847f4331ff0d1af78860ab21de6402b97e340c
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-09 18:27:47 -0800

    add main.py to the project

---
+++ main.py
@@ -0,0 +1,22 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+power_state = True
+
+while power_state:
+    options = menu.get_items()
+    order = input(f"What would you like? ({options}): ")
+    if order == "off":
+        power_state = False
+    elif order == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
